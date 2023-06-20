fbda4641db2dba7bec15208dd3d2875e9eea3c78
Atalykis <cyril1256@hotmail.fr>
2023-06-20 13:55:57 +0200

    (hotfix) keyboard input provider added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { InputsProvider } from './inputs-provider';
+import { InputsProvider, KeyboardInputsProvider } from './inputs-provider';
 import { Graphics } from './graphics/graphics';
 import { MainMenu } from './main-menu';
 import { Physics } from './physics';
@@ -9,13 +9,14 @@ export type Context = 'Menu' | 'Run';
 export class Game {
   private mainMenu: MainMenu;
   public run: Run;
-  private inputsProvider: InputsProvider = new InputsProvider();
+  // private inputsProvider: InputsProvider = new InputsProvider();
+  private inputsProvider: KeyboardInputsProvider = new KeyboardInputsProvider();
   private context: Context = 'Menu';
   constructor(public graphics: Graphics, private physics: Physics) {
     this.mainMenu = new MainMenu(graphics);
     this.run = new Run(this.graphics, this.physics);
     this.switchMode('Menu');
-    this.inputsProvider.mountGamepad();
+    this.inputsProvider.mount();
     setInterval(() => this.tick(), 31);
   }
 
--- src/inputs-provider.ts
+++ src/inputs-provider.ts
@@ -80,3 +80,90 @@ export class InputsProvider {
     return { movement: analogicDirection, action: this.gamepadInputs.action };
   }
 }
+
+export class KeyboardInputsProvider {
+  private isKeyDownPressed: boolean = false;
+  private isKeyUpPressed: boolean = false;
+  private isKeyLeftPressed: boolean = false;
+  private isKeyRightPressed: boolean = false;
+
+  private action = { triggered: false };
+
+  mount() {
+    window.addEventListener('keyup', (e) => {
+      e.preventDefault();
+      switch (e.key) {
+        case 'ArrowDown':
+          this.isKeyDownPressed = false;
+          break;
+        case 'ArrowUp':
+          this.isKeyUpPressed = false;
+          break;
+        case 'ArrowRight':
+          this.isKeyRightPressed = false;
+          break;
+        case 'ArrowLeft':
+          this.isKeyLeftPressed = false;
+          break;
+        case ' ':
+          this.action.triggered = false;
+          break;
+      }
+    });
+
+    window.addEventListener('keydown', (e) => {
+      e.preventDefault();
+      switch (e.key) {
+        case 'ArrowDown':
+          this.isKeyDownPressed = true;
+          break;
+        case 'ArrowUp':
+          this.isKeyUpPressed = true;
+          break;
+        case 'ArrowLeft':
+          this.isKeyLeftPressed = true;
+          break;
+        case 'ArrowRight':
+          this.isKeyRightPressed = true;
+          break;
+        case ' ':
+          this.action.triggered = true;
+          break;
+      }
+    });
+  }
+
+  getInputs() {
+    const direction = this.getAnalogueDirection();
+    return { movement: direction, action: this.action };
+  }
+
+  getAnalogueDirection(): Movement {
+    const isPressingOnlyLeft = this.isKeyLeftPressed && !this.isKeyRightPressed;
+    const isPressingOnlyRight =
+      this.isKeyRightPressed && !this.isKeyLeftPressed;
+    const isPressingOnlyUp = this.isKeyUpPressed && !this.isKeyDownPressed;
+    const isPressingOnlyDown = this.isKeyDownPressed && !this.isKeyUpPressed;
+
+    switch (true) {
+      case isPressingOnlyLeft && isPressingOnlyUp:
+        return { intensity: 1, angle: (3 * Math.PI) / 4 };
+      case isPressingOnlyLeft && isPressingOnlyDown:
+        return { intensity: 1, angle: (5 * Math.PI) / 4 };
+      case isPressingOnlyRight && isPressingOnlyUp:
+        return { intensity: 1, angle: Math.PI / 4 };
+      case isPressingOnlyRight && isPressingOnlyDown:
+        return { intensity: 1, angle: (7 * Math.PI) / 4 };
+      case isPressingOnlyLeft:
+        return { intensity: 1, angle: Math.PI };
+      case isPressingOnlyRight:
+        return { intensity: 1, angle: 0 };
+      case isPressingOnlyDown:
+        return { intensity: 1, angle: (3 * Math.PI) / 2 };
+      case isPressingOnlyUp:
+        return { intensity: 1, angle: Math.PI / 2 };
+      default:
+        return { intensity: 0, angle: Math.PI / 2 };
+    }
+  }
+}
